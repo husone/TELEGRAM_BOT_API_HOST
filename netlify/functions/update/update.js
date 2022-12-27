0ab058bac7d5ffcd04c369b1764d19d941f42f7b
husone <sonhvnde160056@fpt.edu.vn>
2022-12-27 12:26:24 +0700

    fix message

--- netlify/functions/update/update.js
+++ netlify/functions/update/update.js
@@ -4,7 +4,7 @@ const handleMessage = require("../../../handleMessage");
 exports.handler = async (event) => {
     console.log(typeof event)
     console.log(event);
-    const { message } = JSON.parse(event.body);
+    const message = JSON.parse(event.body);
     console.log(message);
     handleMessage(message);
     return { statusCode: 200 };
--- setBot.js
+++ setBot.js
@@ -4,8 +4,10 @@ require('dotenv').config();
 const token = process.env.API_KEY; // get from @BotFather
 const url = `https://${process.env.APP_ID}.netlify.app/.netlify/functions/update`;
 const bot = new TelegramBot(token, { polling: true });
-bot.setWebHook(url);
+console.log(url);
+
 (async () => {
+    await bot.setWebHook(url);
     const info = await bot.getWebHookInfo();
     console.log(info);
     })();
